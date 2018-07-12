9f143cc50c4733d16987620d00710ef535165ae3
mimikun <karaage_nennen@icloud.com>
2018-07-12 21:45:09 +0900

    Add test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -12,5 +17,16 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+    # 演習
+    # https://railstutorial.jp/chapters/updating_and_deleting_users?version=5.1#sec-exercises_user_index
+    # レイアウトにあるすべてのリンクに対して
+    # 統合テストを書いてみましょう。
+    # ログイン済みユーザーとそうでないユーザーの
+    # それぞれに対して、正しい振る舞いを考えてください。
+    # ヒント: log_in_asヘルパーを使って
+    # リスト 5.32にテストを追加してみましょう。
+    log_in_as(@user)
+    get users_path
+    assert_template 'users/index'
   end
 end
