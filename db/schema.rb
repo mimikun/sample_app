550140893433cca9c5ecc66aad2fe2f72f75982c
mimikun <karaage_nennen@icloud.com>
2018-07-23 21:59:41 +0900

    fix typo

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20180718130656) do
     t.boolean  "admin",             default: false
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
-    t.time     "activated_at"
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
