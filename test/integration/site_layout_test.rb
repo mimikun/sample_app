55de786cf301efe591360030e4017c18d65e9499
mimikun <karaage_nennen@icloud.com>
2018-09-12 20:14:53 +0900

    Add Test: layout links when logged in

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -17,16 +17,19 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
-    # 演習
-    # https://railstutorial.jp/chapters/updating_and_deleting_users?version=5.1#sec-exercises_user_index
-    # レイアウトにあるすべてのリンクに対して
-    # 統合テストを書いてみましょう。
-    # ログイン済みユーザーとそうでないユーザーの
-    # それぞれに対して、正しい振る舞いを考えてください。
-    # ヒント: log_in_asヘルパーを使って
-    # リスト 5.32にテストを追加してみましょう。
+  end
+
+  test "layout links when logged in" do
     log_in_as(@user)
-    get users_path
-    assert_template 'users/index'
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
   end
 end
